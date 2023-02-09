67422a82f7b044be28b642bed226d52784b538e4
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2023-02-09 21:32:26 +0100

    started working on ArduinoCLI.java

--- src/main/java/com/firekey/configurator/arduino/ArduinoCLI.java
+++ src/main/java/com/firekey/configurator/arduino/ArduinoCLI.java
@@ -1,4 +1,22 @@
 package com.firekey.configurator.arduino;
 
+import java.util.List;
+import com.fazecast.jSerialComm.SerialPort;
+
 public class ArduinoCLI {
+
+
+    public void upload(String port){
+
+    }
+
+
+    public List<String> getPorts(){
+        return null;
+    }
+
+
+    public void init(){
+
+    }
 }
