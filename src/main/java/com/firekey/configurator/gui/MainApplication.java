99a262578e5ffafaa42fd2353473fc64e360f100
Frederik Wolter <35914049+FrederikWolter@users.noreply.github.com>
2023-02-14 14:08:15 +0100

    started implementing main-view.fxml

--- src/main/java/com/firekey/configurator/gui/MainApplication.java
+++ src/main/java/com/firekey/configurator/gui/MainApplication.java
@@ -4,6 +4,7 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 
 import java.io.IOException;
 
@@ -12,8 +13,9 @@ public class MainApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello FireKey!");
+        Scene scene = new Scene(fxmlLoader.load());     // TODO use 900, 600?
+        stage.setTitle("Hello FireKey!");               // TODO change title
+        stage.initStyle(StageStyle.DECORATED);      // TODO change to undecorated?
         stage.setScene(scene);
         stage.show();
     }
