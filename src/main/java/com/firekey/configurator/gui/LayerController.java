773e10cb0b75ea12307d13f91b2b307d9156a067
Daniel Czeschner <daniel.czeschner@web.de>
2023-02-17 21:54:50 +0100

    get clicked btn idx

--- src/main/java/com/firekey/configurator/gui/LayerController.java
+++ src/main/java/com/firekey/configurator/gui/LayerController.java
@@ -1,4 +1,17 @@
 package com.firekey.configurator.gui;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.Node;
+
 public class LayerController {
+
+    @FXML
+    protected void onMatrixButtonClicked(ActionEvent event) {
+        Node node = (Node) event.getSource();
+        String data = (String) node.getUserData();
+        int buttonIdx = Integer.parseInt(data);
+        System.out.println(buttonIdx);
+    }
+
 }
