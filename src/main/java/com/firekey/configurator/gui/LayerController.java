d245ee721d905703ee5faacdb1569d6a2272b3ee
Daniel Czeschner <daniel.czeschner@web.de>
2023-03-08 21:01:41 +0100

    comments

--- src/main/java/com/firekey/configurator/gui/LayerController.java
+++ src/main/java/com/firekey/configurator/gui/LayerController.java
@@ -15,15 +15,32 @@ import javafx.scene.paint.Color;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+/**
+ * Handling the layer and key-layer configuration
+ */
 public class LayerController implements Initializable {
 
+    // region attributes
+    /**
+     * The function text area
+     */
     @FXML
     private AutoCompleteTextArea taFunctionInput;
+    /**
+     * The default color picker
+     */
+
     @FXML
     private ColorPicker cpDefaultKeyColor;
+    /**
+     * Text field for the key name
+     */
+
     @FXML
     private TextFieldWithLengthLimit tfKeyName;
-
+    /**
+     * Text field for the layer name
+     */
     @FXML
     private TextFieldWithLengthLimit tfLayerName;
 
@@ -37,18 +54,11 @@ public class LayerController implements Initializable {
      */
     private Key currentSelectedKey;
 
-    @FXML
-    protected void onMatrixButtonClicked(ActionEvent event) {
-        // Get the selected key index from the user data
-        Node node = (Node) event.getSource();
-        String data = (String) node.getUserData();
-        int buttonIdx = Integer.parseInt(data);
+    // endregion
 
-        // Set the selected index & update the visuals to this key
-        currentSelectedKey = currentLayer.getKey(buttonIdx);
-        setVisualsToKeyData();
-    }
-
+    /**
+     * Updates the visuals for a selected key
+     */
     private void setVisualsToKeyData() {
         if (currentSelectedKey == null) return;
         // We have a key selected so update the visuals
@@ -66,6 +76,11 @@ public class LayerController implements Initializable {
         }
     }
 
+    /**
+     * Sets the current layer
+     *
+     * @param layer The selected {@link Layer} object
+     */
     public void setLayer(Layer layer) {
         this.currentLayer = layer;
 
@@ -79,22 +94,49 @@ public class LayerController implements Initializable {
         tfLayerName.setText(currentLayer.getName());
     }
 
+    /**
+     * Selects the current clicked key object
+     */
+    @FXML
+    protected void onMatrixButtonClicked(ActionEvent event) {
+        // Get the selected key index from the user data
+        Node node = (Node) event.getSource();
+        String data = (String) node.getUserData();
+        int buttonIdx = Integer.parseInt(data);
+
+        // Set the selected index & update the visuals to this key
+        currentSelectedKey = currentLayer.getKey(buttonIdx);
+        setVisualsToKeyData();
+    }
+
+    /**
+     * Updates the function for a selected key
+     */
     protected void onFunctionTextChanged() {
         if (currentSelectedKey == null || currentSelectedKey.getType() != KeyType.ACTION) return;
         this.currentSelectedKey.setFunction(taFunctionInput.getText());
     }
 
+    /**
+     * Updates the default color for a selected key
+     */
     @FXML
     protected void onColorChanged() {
         if (currentSelectedKey == null) return;
         this.currentSelectedKey.setDefaultColor(cpDefaultKeyColor.getValue());
     }
 
+    /**
+     * Updates the name for a selected key
+     */
     protected void onKeyNameChanged() {
         if (currentSelectedKey == null || currentSelectedKey.getType() != KeyType.ACTION) return;
         this.currentSelectedKey.setName(tfKeyName.getText());
     }
 
+    /**
+     * Updates the name for the selected layer
+     */
     private void onLayerNameChanged() {
         if (currentLayer == null) return;
         this.currentLayer.setName(tfLayerName.getText());
@@ -104,12 +146,14 @@ public class LayerController implements Initializable {
     public void initialize(URL location, ResourceBundle resources) {
         // TODO/CHECK: use focusedProperty and on focus lost?
         taFunctionInput.textProperty().addListener((observable, oldValue, newValue) -> onFunctionTextChanged());
+
         tfKeyName.textProperty().addListener((observable, oldValue, newValue) -> onKeyNameChanged());
         tfKeyName.setMaxLength(8);
+
         tfLayerName.textProperty().addListener((observable, oldValue, newValue) -> onLayerNameChanged());
         tfLayerName.setMaxLength(10);
 
-
+        // Add auto complete items to the function text area
         // TODO add entries
         taFunctionInput.addAutoCompleteEntry("Keyboard.press();")
                 .addAutoCompleteEntry("KEY_LEFT_CTRL")
