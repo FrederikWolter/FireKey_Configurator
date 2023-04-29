ad93eaa6a5f048b2cba5f56fd4ef8d984f8c6469
Daniel Czeschner <daniel.czeschner@web.de>
2023-04-29 10:15:20 +0200

    comment updates

--- src/main/java/com/firekey/configurator/gui/CommandController.java
+++ src/main/java/com/firekey/configurator/gui/CommandController.java
@@ -2,7 +2,12 @@ package com.firekey.configurator.gui;
 
 import javafx.scene.control.TextArea;
 
+/**
+ * Handles the command output window
+ */
 public class CommandController {
+    // region javafx-elements
     @javafx.fxml.FXML
     private TextArea taCliOutput;
+    // endregion
 }
