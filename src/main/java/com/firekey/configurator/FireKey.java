5877ae9c68c1bcf51fbad6a99b75c1f354809d83
Daniel Czeschner <daniel.czeschner@web.de>
2023-04-29 09:36:17 +0200

    changed version

--- src/main/java/com/firekey/configurator/FireKey.java
+++ src/main/java/com/firekey/configurator/FireKey.java
@@ -8,7 +8,7 @@ import javafx.application.Application;
  */
 public class FireKey {
 
-    public static final String VERSION = "V0.5.2";
+    public static final String VERSION = "V0.6.2";
 
     public static void main(String[] args) {
         Application.launch(MainApplication.class, args);
